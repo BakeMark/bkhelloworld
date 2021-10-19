760ea518840ce25fc73b6725ebf970a3f4e89dba
lou-viannay <lou.viannay.88@gmail.com>
2021-10-20 05:59:05 +0800

    move docker files
    Signed-off-by: lou-viannay <lou.viannay.88@gmail.com>

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 if [ "x$DJANGO_MANAGEPY_MIGRATE" == 'xon' ]; then
+  echo "executing django migrate..."
   python manage.py migrate --noinput
+else
+  echo "skipping django migrate..."
 fi
 
 exec "$@"
